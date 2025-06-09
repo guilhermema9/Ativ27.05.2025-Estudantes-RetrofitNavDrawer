e9d8a199816128fca5451a50281f25cbd8a2ba9e
guilhermema9 <guilherme.ma9@gmail.com>
2025-06-09 20:48:34 -0300

    fix: corrige navegação do Navigation Drawer

--- app/src/main/java/com/example/ativ27052025_estudantes_retrofitnavdrawer/MainActivity.java
+++ app/src/main/java/com/example/ativ27052025_estudantes_retrofitnavdrawer/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_estatistica, R.id.nav_cadastra_estudante, R.id.nav_estudante)
+                R.id.nav_estatistica, R.id.nav_cadastra_estudante, R.id.nav_lista_estudantes)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
